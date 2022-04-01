476d4bef3d6a3f128534ab55d074d940adcfe2cb
GJD <64826216+GJD-first@users.noreply.github.com>
2022-04-01 13:30:57 +0800

    complete LineTree (use array to store)

--- LineTree.java
+++ LineTree.java
@@ -1,32 +1,46 @@
 package datastruct;
-
+//calucate sum of range
+ 
 public class LineTree {
-	public int left, right, value;
-	public LineTree lTree, rTree;
-
-	public LineTree() {
-		// TODO Auto-generated constructor stub
-		lTree = null;
-		rTree = null;
+	int[] tr;
+	int n;
+	LineTree(int[] a)
+	{
+		n=a.length;
+		tr=new int[4*n];
+		build(0,0,n-1,a);
 	}
-
-	public LineTree(int low, int high) {
-		left = low;
-		right = high;
-		lTree = null;
-		rTree = null;
-	}
-
-	public static LineTree build(int a[], int low, int high) {
-		LineTree node = new LineTree(a[low], a[high]);
-		if (low == high) {
-			node.value = a[low];
-		} else {
-			int mid = low + (high - low) / 2;
-			node.lTree = build(a, low, mid);
-			node.rTree = build(a, mid + 1, high);
-			node.value = node.lTree.value + node.rTree.value;
+	private build(int id,int left,int right,int[] a)
+	{
+		if(right<left)return;
+		if(left==right)
+		{
+			tr[id]=a[left];
+			return;
 		}
-		return node;
+		int mid=left+(right-left)/2;
+		build(id*2+1,left,mid,a);
+		build(id*2+1,mid+1,right,a);
+		tr[id]=tr[id*2+1]+tr[id*2+2];
 	}
+	private int msearch(int id,int left,int right,int aleft,int aright)
+	{
+		if(aleft>=left&&aright<=right)return tr[id];
+		int mid=aleft+(aright-aleft)/2;
+		if(mid<=right)return msearch(id*2+1,left,right,aleft,mid);
+		if(left>mid) return msearch(id*2+2,left,right,mid+1,aright);
+		return msearch(id*2+1,left,right,aleft,mid)+msearch(id*2+2,left,right,mid+1,aright);
+	}
+	public int search(int left,int right)
+	{
+		return msearch(0,left,right,0,n-1);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
